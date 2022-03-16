1f22b9f3afa855296fc2e603dac49d9dd09e9bae
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-16 12:08:59 +0100

    add ScreenReaderOnly and Button

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,30 +1,53 @@
 import styled from 'styled-components';
+import ScreenReaderOnly from './ScreenReaderOnly.js';
 
-export default function EntryForm() {
+export default function EntryForm({ onSubmit }) {
   return (
-    <Form onSubmit={handleSubmit}>
-      <label>Enter your name:</label>
+    <Form onSubmit={handleSubmit} aria-labelledby="entry-form-name">
+      <label htmlFor="text">
+        <ScreenReaderOnly>Entry text</ScreenReaderOnly>
+      </label>
       <input
+        name="text"
+        id="text"
+        placeholder="Just some text ..."
         type="text"
-        name="name"
-        id="name"
-        placeholder="enter your name here..."
-        required
-      ></input>
-      <button type="submit">submit</button>
+      />
+      <PlusButton id="entry-form-name">
+        <ScreenReaderOnly>Create new entry</ScreenReaderOnly>
+        <div aria-hidden="true">+</div>
+      </PlusButton>
     </Form>
   );
 
   function handleSubmit(event) {
     event.preventDefault();
+    const form = event.target;
+    const inputElement = form.elements.text;
+    onSubmit(inputElement.value);
   }
 }
 
 const Form = styled.form`
   display: flex;
-  flex-direction: column;
-  gap: 20px;
-  border-radius: 5px;
+  flex-direction: row;
+  justify-content: space-around;
   padding: 20px;
+  border-radius: 5px;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+
+  input {
+    width: 100%;
+    margin-right: 20px;
+  }
+`;
+
+const PlusButton = styled.button`
+  border-radius: 50%;
+  border: 0;
+  padding: 5px 10px;
+
+  div {
+    margin-top: -1px;
+  }
 `;
