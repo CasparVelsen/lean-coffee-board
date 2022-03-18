fc0949fafb324fcc4cbbc8f6ff83cee8f900fb7e
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-18 10:48:59 +0100

    Push for vercel

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -14,7 +14,7 @@ const Card = styled.section`
   flex-direction: column;
   gap: 20px;
   padding: 20px;
-  width: 300px;
+  width: 350px;
   height: fit-content;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
     rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
